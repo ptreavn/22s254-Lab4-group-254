9ff8663af8ff39b8c3abf6ac66d423edb2e7a622
ptreavn <46984481+ptreavn@users.noreply.github.com>
2022-09-15 19:14:33 -0700

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,16 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
 
+void readfile(ifstream& infile);
+
+void readfile(ifstream& infile) {
+    if (infile.is_open()) {
+        char mychar;
+        while (infile.good()) {
+            mychar = infile.get();
+            cout << mychar;
+        }
+    }
+    
+}
